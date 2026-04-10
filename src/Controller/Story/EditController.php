c4964d9f643d359244cac0bb6817446491c01a1f
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-10 20:41:49 +0100

    feat: Create new PSR story controllers

---
+++ src/Controller/Story/EditController.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2003-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://cvs.horde.org/co.php/jonah/LICENSE.
+ */
+
+namespace Horde\Jonah\Controller\Story;
+
+use Exception;
+use Horde;
+use Horde\Jonah\Service\PermissionChecker;
+use Horde\Jonah\Traits\ResponseTrait;
+use Horde_Exception_AuthenticationFailure;
+use Horde_Notification_Handler;
+use Horde_PageOutput;
+use Horde_Perms;
+use Horde_Registry;
+use Horde_Variables;
+use Jonah_Driver;
+use Jonah_Form_Story;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * PSR-15 controller for creating and editing stories.
+ *
+ * Replaces stories/edit.php + Jonah_View_StoryEdit.
+ *
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Jonah
+ */
+class EditController implements RequestHandlerInterface
+{
+    use ResponseTrait;
+
+    public function __construct(
+        private readonly Jonah_Driver $driver,
+        private readonly PermissionChecker $permissions,
+        private readonly Horde_Notification_Handler $notification,
+        private readonly Horde_PageOutput $pageOutput,
+        private readonly Horde_Registry $registry,
+    ) {}
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $vars = Horde_Variables::getDefaultVariables();
+
+        $channel_id = $vars->get('channel_id');
+
+        try {
+            $channel = $this->driver->getChannel($channel_id);
+        } catch (Exception $e) {
+            $this->notification->push(
+                sprintf(_("Story editing failed: %s"), $e->getMessage()),
+                'horde.error',
+            );
+            return $this->redirect((string) Horde::url('channels/index.php', true));
+        }
+
+        if (!$this->permissions->check('channels', Horde_Perms::EDIT, [$channel_id])) {
+            $this->notification->push(
+                _("You are not authorised for this action."),
+                'horde.warning',
+            );
+            throw new Horde_Exception_AuthenticationFailure();
+        }
+
+        $story_id = $vars->get('id');
+        if ($story_id && !$vars->get('formname')) {
+            try {
+                $story = $this->driver->getStory($story_id);
+                $story['tags'] = implode(',', array_values($story['tags'] ?? []));
+                $vars = new Horde_Variables($story);
+            } catch (Exception $e) {
+                $this->notification->push(
+                    sprintf(_("Error loading story: %s"), $e->getMessage()),
+                    'horde.error',
+                );
+                return $this->redirect(
+                    (string) Horde::url('stories/index.php')->add('channel_id', $channel_id),
+                );
+            }
+        }
+
+        $form = new Jonah_Form_Story($vars);
+        if ($form->validate($vars)) {
+            $info = $form->getInfo($vars);
+            $info['author'] = $this->registry->getAuth();
+            try {
+                $this->driver->saveStory($info);
+                $this->notification->push(
+                    sprintf(_("The story \"%s\" has been saved."), $info['title']),
+                    'horde.success',
+                );
+                return $this->redirect(
+                    (string) Horde::url('stories/index.php')->add('channel_id', $channel_id),
+                );
+            } catch (Exception $e) {
+                $this->notification->push(
+                    sprintf(_("There was an error saving the story: %s"), $e->getMessage()),
+                    'horde.error',
+                );
+            }
+        }
+
+        $html = $this->renderChrome($form->getTitle(), function () use ($form, $vars) {
+            $form->renderActive(
+                $form->getRenderer(),
+                $vars,
+                Horde::url('stories/edit.php'),
+                'post',
+            );
+        });
+
+        return $this->htmlResponse($html);
+    }
+}
---
+++ src/Controller/Story/ViewController.php
@@ -0,0 +1,215 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2003-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://cvs.horde.org/co.php/jonah/LICENSE.
+ */
+
+namespace Horde\Jonah\Controller\Story;
+
+use Exception;
+use Horde;
+use Horde\Jonah\Traits\ResponseTrait;
+use Horde_Browser;
+use Horde_Core_Ui_TagCloud;
+use Horde_Notification_Handler;
+use Horde_PageOutput;
+use Horde_Registry;
+use Horde_Text_Filter_Text2html;
+use Horde_View;
+use Jonah_Driver;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * PSR-15 controller for viewing a single story.
+ *
+ * Replaces stories/view.php + Jonah_View_StoryView.
+ *
+ * @category Horde
+ * @license  http://www.horde.org/licenses/bsd BSD
+ * @package  Jonah
+ */
+class ViewController implements RequestHandlerInterface
+{
+    use ResponseTrait;
+
+    public function __construct(
+        private readonly Jonah_Driver $driver,
+        private readonly Horde_Notification_Handler $notification,
+        private readonly Horde_PageOutput $pageOutput,
+        private readonly Horde_Registry $registry,
+        private readonly Horde_Browser $browser,
+        private readonly LoggerInterface $logger,
+    ) {}
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $queryParams = $request->getQueryParams();
+        $channel_id = $queryParams['channel_id'] ?? null;
+        $story_id = $queryParams['id'] ?? null;
+
+        if (!$story_id) {
+            try {
+                $story_id = $this->driver->getLatestStoryId($channel_id);
+            } catch (Exception $e) {
+                $this->notification->push(
+                    sprintf(_("Error fetching story: %s"), $e->getMessage()),
+                    'horde.warning',
+                );
+                $html = $this->renderChrome(_("Story"), function () {});
+                return $this->htmlResponse($html);
+            }
+        }
+
+        try {
+            $story = $this->driver->getStory($story_id, !$this->browser->isRobot());
+        } catch (Exception $e) {
+            $this->notification->push(
+                sprintf(_("Error fetching story: %s"), $e->getMessage()),
+                'horde.warning',
+            );
+            $html = $this->renderChrome(_("Story"), function () {});
+            return $this->htmlResponse($html);
+        }
+
+        /* Syntax highlighter setup */
+        $this->pageOutput->addScriptFile('syntaxhighlighter/scripts/shCore.js', 'horde');
+        $this->pageOutput->addScriptFile('syntaxhighlighter/scripts/shAutoloader.js', 'horde');
+        $path = $this->registry->get('jsuri', 'horde') . '/syntaxhighlighter/scripts/';
+        $brushes = <<<EOT
+                      SyntaxHighlighter.autoloader(
+                      'applescript            {$path}shBrushAppleScript.js',
+                      'actionscript3 as3      {$path}shBrushAS3.js',
+                      'bash shell             {$path}shBrushBash.js',
+                      'coldfusion cf          {$path}shBrushColdFusion.js',
+                      'cpp c                  {$path}shBrushCpp.js',
+                      'c# c-sharp csharp      {$path}shBrushCSharp.js',
+                      'css                    {$path}shBrushCss.js',
+                      'delphi pascal          {$path}shBrushDelphi.js',
+                      'diff patch pas         {$path}shBrushDiff.js',
+                      'erl erlang             {$path}shBrushErlang.js',
+                      'groovy                 {$path}shBrushGroovy.js',
+                      'java                   {$path}shBrushJava.js',
+                      'jfx javafx             {$path}shBrushJavaFX.js',
+                      'js jscript javascript  {$path}shBrushJScript.js',
+                      'perl pl                {$path}shBrushPerl.js',
+                      'php                    {$path}shBrushPhp.js',
+                      'text plain             {$path}shBrushPlain.js',
+                      'py python              {$path}shBrushPython.js',
+                      'ruby rails ror rb      {$path}shBrushRuby.js',
+                      'sass scss              {$path}shBrushSass.js',
+                      'scala                  {$path}shBrushScala.js',
+                      'sql                    {$path}shBrushSql.js',
+                      'vb vbnet               {$path}shBrushVb.js',
+                      'xml xhtml xslt html    {$path}shBrushXml.js'
+                    );
+            EOT;
+        $this->pageOutput->addInlineScript([
+            $brushes,
+            'SyntaxHighlighter.defaults[\'toolbar\'] = false',
+            'SyntaxHighlighter.all()',
+        ], true);
+
+        $sh_js_fs = $this->registry->get('jsfs', 'horde') . '/syntaxhighlighter/styles/';
+        $sh_js_uri = Horde::url($this->registry->get('jsuri', 'horde'), false, -1)
+            . '/syntaxhighlighter/styles/';
+        $this->pageOutput->addStylesheet(
+            $sh_js_fs . 'shCoreEclipse.css',
+            $sh_js_uri . 'shCoreEclipse.css',
+        );
+        $this->pageOutput->addStylesheet(
+            $sh_js_fs . 'shThemeEclipse.css',
+            $sh_js_uri . 'shThemeEclipse.css',
+        );
+
+        /* Tag cloud for the channel */
+        $cloud = new Horde_Core_Ui_TagCloud();
+        $allTags = $this->driver->listTagInfo($channel_id);
+        foreach ($allTags as $tag_id => $taginfo) {
+            $cloud->addElement(
+                $taginfo['tag_name'],
+                Horde::url('stories/results.php')->add([
+                    'tag' => trim($taginfo['tag_name']),
+                    'channel_id' => $channel_id,
+                ]),
+                $taginfo['count'],
+            );
+        }
+
+        /* Filter story content */
+        if (!empty($story['body_type']) && $story['body_type'] === 'text') {
+            $story['body'] = $GLOBALS['injector']
+                ->getInstance('Horde_Core_Factory_TextFilter')
+                ->filter(
+                    $story['body'],
+                    'text2html',
+                    ['parselevel' => Horde_Text_Filter_Text2html::MICRO],
+                );
+        }
+
+        if (!empty($story['url'])) {
+            $story['body'] .= Horde::link(Horde::externalUrl($story['url']))
+                . htmlspecialchars($story['url']) . '</a></p>';
+        }
+
+        if (empty($story['published_date'])) {
+            $story['published_date'] = false;
+        }
+
+        $view = new Horde_View(['templatePath' => [
+            JONAH_TEMPLATES . '/stories',
+            JONAH_TEMPLATES . '/stories/partial',
+            JONAH_TEMPLATES . '/stories/layout',
+        ]]);
+        $view->addHelper('Tag');
+        $view->addHelper('Text');
+        $view->tagcloud = $cloud->buildHTML();
+        $view->story = $story;
+
+        $conf = $GLOBALS['conf'];
+
+        /* Sharing link */
+        if (!empty($conf['sharing']['allow'])) {
+            $url = Horde::url('stories/share.php')->add([
+                'id' => $story['id'],
+                'channel_id' => $channel_id,
+            ]);
+            $view->sharelink = $url->link() . _("Share this story") . '</a>';
+        }
+
+        /* Comments */
+        if (!empty($conf['comments']['allow'])) {
+            if (!$this->registry->hasMethod('forums/doComments')) {
+                $this->logger->error(
+                    'User comments are enabled but the forums API is not available.',
+                );
+            } else {
+                try {
+                    $comments = $this->registry->call(
+                        'forums/doComments',
+                        ['jonah', $story_id, 'commentCallback'],
+                    );
+                } catch (Exception $e) {
+                    $this->logger->error('Error fetching comments: {error}', [
+                        'error' => $e->getMessage(),
+                    ]);
+                    $comments = ['threads' => '', 'comments' => ''];
+                }
+                $view->comments = $comments;
+            }
+        }
+
+        $html = $this->renderChrome($story['title'] ?? _("Story"), function () use ($view) {
+            echo $view->render('view');
+        });
+
+        return $this->htmlResponse($html);
+    }
+}
