19e43627e12f5bd8e5abb3740658a8dd879d65e8
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-11 09:55:40 +0200

    refactor: PSR-7 migration for delivery and dispatcher

--- dispatcher.php
+++ dispatcher.php
@@ -24,33 +24,44 @@ use Horde\Http\StreamFactory;
 use Horde\Http\UriFactory;
 use Horde\Http\Server\RequestBuilder;
 use Horde\Http\Server\ResponseWriterWeb;
+use Horde\Routes\Mapper;
 
-$m = new Horde_Routes_Mapper();
+$mapper = new Mapper();
 
 require JONAH_BASE . '/config/routes.php';
 if (file_exists(JONAH_BASE . '/config/routes.local.php')) {
     include JONAH_BASE . '/config/routes.local.php';
 }
 
-$httpRequest = new Horde_Controller_Request_Http();
-$url = $httpRequest->getPath();
-$result = $m->match('/' . $url);
-
-if (!$result) {
-    $GLOBALS['notification']->push(_("Page not found."), 'horde.error');
-    Horde::url('channels/index.php', true)->redirect();
-    exit;
-}
-
 /*
- * Build a PSR-7 request. The matched route result and query params are
- * available to the controller via query parameters and nonInputVar.
+ * Build PSR-7 request and extract the URL path to match against routes.
+ * Strip the Jonah webroot prefix so the mapper sees paths relative to the app.
  */
 $request = (new RequestBuilder(
     new RequestFactory(),
     new StreamFactory(),
     new UriFactory(),
 ))->withGlobalVariables()->build();
+
+$webroot = rtrim($GLOBALS['registry']->get('webroot', 'jonah'), '/');
+$path = $request->getUri()->getPath();
+if ($webroot !== '' && str_starts_with($path, $webroot)) {
+    $path = substr($path, strlen($webroot));
+}
+if ($path === '' || $path === false) {
+    $path = '/';
+}
+
+$result = $mapper->match($path);
+
+if (!$result) {
+    $urlGenerator = $GLOBALS['injector']->getInstance(
+        Horde\Jonah\Service\UrlGenerator::class,
+    );
+    $GLOBALS['notification']->push(_("Page not found."), 'horde.error');
+    header('Location: ' . $urlGenerator->absoluteUrlFor('ChannelList'));
+    exit;
+}
 
 switch ($result['controller']) {
     case 'admin':
@@ -68,11 +79,15 @@ switch ($result['controller']) {
             $response = $controller->handle($request);
             (new ResponseWriterWeb())->writeResponse($response);
         } else {
+            $urlGenerator = $GLOBALS['injector']->getInstance(
+                Horde\Jonah\Service\UrlGenerator::class,
+            );
             $GLOBALS['notification']->push(
                 sprintf(_("Unknown controller: %s"), $controllerClass),
                 'horde.error',
             );
-            Horde::url('channels/index.php', true)->redirect();
+            header('Location: ' . $urlGenerator->absoluteUrlFor('ChannelList'));
+            exit;
         }
         break;
 }
