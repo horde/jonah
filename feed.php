be1a94d964e26721787cf8778acf3b3c822b1c9f
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-11 06:23:16 +0200

    refactor: feed.php endpoint is now just a wrapper around HtmlController

--- feed.php
+++ feed.php
@@ -1,79 +1,34 @@
 <?php
 
 /**
- * Script to handle requests for html delivery of stories.
+ * Legacy entry point for HTML feed delivery — delegates to HtmlController.
  *
  * Copyright 2004-2026 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (BSD). If you did not
  * did not receive this file, see http://cvs.horde.org/co.php/jonah/LICENSE.
- *
- * @author Jan Schneider <jan@horde.org>
  */
 
 require_once __DIR__ . '/lib/Application.php';
-$jonah = Horde_Registry::appInit('jonah', [
+Horde_Registry::appInit('jonah', [
     'authentication' => 'none',
     'session_control' => 'readonly',
 ]);
-$jonah = Horde_Registry::appInit('jonah');
-/**
- * ARCHITECTURE VIOLATION: Using deprecated Horde::loadConfiguration()
- * @deprecated Use $registry->loadConfigFile() instead
- * @see Horde_Deprecated::loadConfiguration()
- */
-$templates = Horde::loadConfiguration('templates.php', 'templates', 'jonah');
 
-/* Get the id and format of the feed to display. */
-$criteria = Horde_Util::nonInputVar('criteria');
-if (empty($criteria['channel_format'])) {
-    // Select the default channel format
-    $criteria['channel_format'] = key($templates);
-}
+use Horde\Http\RequestFactory;
+use Horde\Http\StreamFactory;
+use Horde\Http\UriFactory;
+use Horde\Http\Server\RequestBuilder;
+use Horde\Http\Server\ResponseWriterWeb;
+use Horde\Jonah\Controller\Feed\HtmlController;
 
-$options = [];
-foreach ($templates as $key => $info) {
-    $options[] = '<option value="' . $key . '"' . ($key == $criteria['channel_format'] ? ' selected="selected"' : '') . '>' . $info['name'] . '</option>';
-}
+$request = (new RequestBuilder(
+    new RequestFactory(),
+    new StreamFactory(),
+    new UriFactory(),
+))->withGlobalVariables()->build();
 
-if (empty($criteria['channel_id']) && !empty($criteria['feed'])) {
-    $criteria['channel_id'] = $GLOBALS['injector']->getInstance('Jonah_Driver')->getChannelId($criteria['feed']);
-}
+$controller = $GLOBALS['injector']->getInstance(HtmlController::class);
+$response = $controller->handle($request);
 
-if (empty($criteria['channel_id'])) {
-    $notification->push(_("No valid feed name or ID requested."), 'horde.error');
-} else {
-    $stories = $GLOBALS['injector']->getInstance('Jonah_Driver')->getStories($criteria);
-}
-
-if (!empty($stories)) {
-    // @TODO: Implement proper story output
-}
-
-$view = new Horde_View(['templatePath' => JONAH_TEMPLATES . '/delivery']);
-$view->url = Horde::selfUrl();
-$view->session = Horde_Util::formInput();
-$view->channel_id = $criteria['channel_id'] ?? '';
-$driver = $GLOBALS['injector']->getInstance('Jonah_Driver');
-try {
-    $channel = $driver->getChannel($criteria['channel_id']);
-    $view->channel_name = $channel['channel_name'];
-} catch (Exception $e) {
-    $view->channel_name = '';
-}
-$view->format = $criteria['channel_format'];
-$view->options = $options;
-try {
-    $view->stories = $driver->renderChannel($criteria['channel_id'], $criteria['channel_format']);
-} catch (Exception $e) {
-    $view->stories = '';
-}
-
-// Buffer the notifications and send to the template
-Horde::startBuffer();
-$GLOBALS['notification']->notify(['listeners' => 'status']);
-$view->notify = Horde::endBuffer();
-
-$page_output->header();
-echo $view->render('html');
-$page_output->footer();
+(new ResponseWriterWeb())->writeResponse($response);
