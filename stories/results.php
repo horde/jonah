2af32ca95486f3807769f34ae7afa6d0e9123809
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-11 07:22:41 +0200

    refactor: stories.php now delegates to TagSearchController

--- stories/results.php
+++ stories/results.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Display list of articles that match a tag query.
+ * Legacy entry point for tag search results — delegates to TagSearchController.
  *
  * Copyright 2003-2026 Horde LLC (http://www.horde.org/)
  *
@@ -9,27 +9,26 @@
  * did not receive this file, see http://cvs.horde.org/co.php/jonah/LICENSE.
  *
  * @author Michael J. Rubinsky <mrubinsk@horde.org>
- * @package Jonah
+ * @author Ralf Lang <ralf.lang@ralf-lang.de>
  */
+
 require_once __DIR__ . '/../lib/Application.php';
 Horde_Registry::appInit('jonah');
-$driver = $GLOBALS['injector']->getInstance('Jonah_Driver');
 
-/* Make sure we actually requested a tag search */
-$tag = Horde_Util::getFormData('tag');
-if (empty($tag)) {
-    $notification->push(_("No tag requested."), 'horde.error');
-    Horde::url('channels/index.php', true)->redirect();
-}
+use Horde\Http\RequestFactory;
+use Horde\Http\StreamFactory;
+use Horde\Http\UriFactory;
+use Horde\Http\Server\RequestBuilder;
+use Horde\Http\Server\ResponseWriterWeb;
+use Horde\Jonah\Controller\TagSearchController;
 
-// @todo - use an actual Horde_View
-$params = [
-    'registry' => $registry,
-    'notification' => $notification,
-    'prefs' => $prefs,
-    'conf' => $conf,
-    'tag' => $tag,
-    'channel_id' => Horde_Util::getFormData('channel_id'),
-];
-$view = new Jonah_View_TagSearchList($params);
-$view->run();
+$request = (new RequestBuilder(
+    new RequestFactory(),
+    new StreamFactory(),
+    new UriFactory(),
+))->withGlobalVariables()->build();
+
+$controller = $GLOBALS['injector']->getInstance(TagSearchController::class);
+$response = $controller->handle($request);
+
+(new ResponseWriterWeb())->writeResponse($response);
