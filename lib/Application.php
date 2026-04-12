a88f18074a1df57fc53e58006ad6a602c5ecbf2a
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-12 20:29:03 +0200

    chore(release): bump version to 1.0.0-beta3
    Release version 1.0.0-beta3
    
    refactor: Use modern Horde\Util\Util class where possible
    refactor: Use ResponsiveAssets for resolving theme images

--- lib/Application.php
+++ lib/Application.php
@@ -28,7 +28,7 @@ require_once HORDE_BASE . '/lib/core.php';
 
 class Jonah_Application extends Horde_Registry_Application
 {
-    public $version = '1.0.0-beta2';
+    public $version = '1.0.0-beta3';
 
     /**
      */
