3fd4df798db104ec3f03a58d27e6f147be43ae0c
Ralf Lang <ralf.lang@ralf-lang.de>
2026-06-16 12:31:01 +0200

    chore(release): bump version to 1.0.0-RC2
    Release version 1.0.0-RC2
    
    style: php-cs-fixer
    refactor(routes): resolve RegistryState via RegistryConfigLoader
    refactor(routes): replace Mapper with RoutesProvider in UrlGenerator and test fixtures
    docs(UrlGenerator): comment future migration to RouteUrlWriter
    chore: Update workflow dependency

--- lib/Application.php
+++ lib/Application.php
@@ -28,7 +28,7 @@ require_once HORDE_BASE . '/lib/core.php';
 
 class Jonah_Application extends Horde_Registry_Application
 {
-    public $version = '1.0.0-RC1';
+    public $version = '1.0.0-RC2';
 
     /**
      */
